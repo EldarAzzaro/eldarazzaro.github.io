ab04898e8b7a2de22a24ea39796522999fc709ae
EldarAzzaro <61908243+EldarAzzaro@users.noreply.github.com>
2020-03-17 09:52:47 +0300

    Bootstrap project
    chaged some elemts

---
+++ BOOTSTRAP/javascript/main.js
@@ -0,0 +1,15 @@
+
+// let obj = {
+// 	name: 'Eldar',
+// 	age: 25,
+// 	isHuman: true,
+// } 
+
+// let translator = {
+// 	"яблоко": "apple"
+// }
+
+// console.log('obj')
+
+var a=10;
+console.log(a++)
